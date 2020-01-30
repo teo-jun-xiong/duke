2e9e528cb8d9b069bc4d8bb418c68751f446fd9c
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-01-30 20:20:59 +0800

    Add test code for EventTask

--- src/test/java/DeadlineTaskTest.java
+++ src/test/java/DeadlineTaskTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
@@ -6,25 +5,25 @@ import java.text.ParseException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class DeadlineTaskTest {
+class DeadlineTaskTest {
 
     @Test
-    public void testCreateDeadlineTask_success() throws ParseException {
+    void testCreateDeadlineTask_success() throws ParseException {
         // 1234 becomes 0034
         assertEquals("[D] [☓] Buy birthday present (by: Sat 2345, Sep 9th, 2000)",
                 DeadlineTask.createDeadlineTask("Buy birthday present", "09/09/2000 2345").toString());
     }
 
     @Test
-    public void testCreateDeadlineTask_invalidDate_exceptionThrown() throws ParseException {
-       try {
-           assertEquals("[D] [☓] Buy birthday present (by: Sat 2345, Sep 9th, 2000)",
-                   DeadlineTask.createDeadlineTask("Buy birthday present", "09-09-2000 2345").toString());
-           fail();
-       } catch (InvalidTaskDateTimeException e) {
+    void testCreateDeadlineTask_invalidDate_exceptionThrown() throws ParseException {
+        try {
+            assertEquals("[D] [☓] Buy birthday present (by: Sat 2345, Sep 9th, 2000)",
+                    DeadlineTask.createDeadlineTask("Buy birthday present", "09-09-2000 2345").toString());
+            fail();
+        } catch (InvalidTaskDateTimeException e) {
             assertEquals("   The date and/or time format is invalid.\n"
                     + "   Please format your input as: DD/MM/YYYY HHmm.", e.getMessage());
-       }
+        }
 
     }
 }
---
+++ src/test/java/EventTaskTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class EventTaskTest {
+
+    @Test
+    void testCreateEventTask_success() throws ParseException {
+        // 1234 becomes 0034
+        assertEquals("[E] [☓] Birthday (at: Sat 2345, Sep 9th, 2000)",
+                EventTask.createEventTask("Birthday", "09/09/2000 2345").toString());
+    }
+
+    @Test
+    void testCreateEventTask_invalidDate_exceptionThrown() throws ParseException {
+        try {
+            assertEquals("[E] [☓] Birthday (by: Sat 2345, Sep 9th, 2000)",
+                    EventTask.createEventTask("Birthday", "09-09-2000 2345").toString());
+            fail();
+        } catch (InvalidTaskDateTimeException e) {
+            assertEquals("   The date and/or time format is invalid.\n"
+                    + "   Please format your input as: DD/MM/YYYY HHmm.", e.getMessage());
+        }
+
+    }
+}
